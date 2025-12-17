dc11774d47e995813ab81c8f002fc52bcf6b64aa
bala-amypo <bala@amypo.in>
2025-12-17 10:00:08 +0000

    User commit

--- src/main/java/com/example/demo/Repository/StudentRepo.java
+++ src/main/java/com/example/demo/Repository/StudentRepo.java
@@ -1,8 +1,10 @@
 package com.example.demo.repository;
-import com.example.demo.Entity.Student;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
-public interface StudentRepo extends JpaRepository<Student,Integer> {
+public interface Studentrepo extends JpaRepository<Student,Integer>{
+
 }
